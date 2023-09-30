3f90598298c34bf8e5296b0233c920cffd3da4dc
Hiroya Enomoto <mysdrikasui@gmail.com>
2023-09-30 16:04:34 +0900

    GamePageにタイマーを結合

--- lib/pages/game/widgets/timer_bar_wrapper.dart
+++ lib/pages/game/widgets/timer_bar_wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter_hackathon_team_a/pages/game/feature/timer.dart';
-import 'package:flutter_hackathon_team_a/pages/game/feature/timer_state.dart';
+import 'package:flutter_hackathon_team_a/pages/game/feature/game.dart';
+import 'package:flutter_hackathon_team_a/pages/game/feature/game_state.dart';
 import 'package:flutter_hackathon_team_a/pages/game/widgets/animated_timer_bar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -19,24 +19,18 @@ class _TimerBarWrapperState extends ConsumerState<TimerBarWrapper>
   void initState() {
     super.initState();
     SchedulerBinding.instance.addPostFrameCallback((_) {
-      ref.read(timerProvider.notifier).initializeController(this);
+      ref.read(gameProvider.notifier).initializeAnimationController(this);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final timerState = ref.watch(timerProvider);
-    return Column(
-      children: [
-        Center(
-          child: timerState.isInitialized
-              ? AnimatedTimerBar(
-                  animationController: timerState.animationController!)
-              : const Text('準備中...'),
-        ),
-        FloatingActionButton(
-            onPressed: ref.read(timerProvider.notifier).reduceTimer),
-      ],
+    final gameState = ref.watch(gameProvider);
+    return Center(
+      child: gameState.isAnimationControllerInitialized
+          ? AnimatedTimerBar(
+              animationController: gameState.animationController!)
+          : const Text('準備中...'),
     );
   }
 }
